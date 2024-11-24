52c867e29a6f604693cf3011640e79eefe29b7de
Kiprotich Code <fleeklawi@gmail.com>
2024-11-24 12:32:54 +0300

    update url route for slider image

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,5 +8,5 @@ router = DefaultRouter()
 router.register(r'slider_images', ImageViewSet, basename='slider_image')
 
 urlpatterns = [
-    path('slider_images/', include(router.urls)),
+    path('', include(router.urls)),
 ]
